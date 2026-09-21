fc42310f784ba43a9eb4206c7b7df45b13b3e910
Josue Reyes-Melendez <josureyes1420@gmail.com>
2026-09-21 19:36:10 -0400

    Remove target build directory from tracking

--- lab_3tasks/target/debug/deps/lab_3tasks-042fa8739a61a09b.d
+++
@@ -1,5 +0,0 @@
-/Users/josuereyes/Coding/School Coding/COSC-455/hello_world/lab_3tasks/target/debug/deps/lab_3tasks-042fa8739a61a09b.d: src/main.rs
-
-/Users/josuereyes/Coding/School Coding/COSC-455/hello_world/lab_3tasks/target/debug/deps/lab_3tasks-042fa8739a61a09b: src/main.rs
-
-src/main.rs:
--- lab_3tasks/target/debug/lab_3tasks.d
+++
@@ -1,1 +0,0 @@
-/Users/josuereyes/Coding/School\ Coding/COSC-455/hello_world/lab_3tasks/target/debug/lab_3tasks: /Users/josuereyes/Coding/School\ Coding/COSC-455/hello_world/lab_3tasks/src/main.rs
